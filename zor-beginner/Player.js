7a3ce9651cfea7c16cde41d9eedc176ecc121ac8
Christophe Gasnier <chrisg93@hotmail.com>
2015-05-23 18:34:32 +0200

    deb-3

--- zor-beginner/Player.js
+++ zor-beginner/Player.js
@@ -1,11 +1,23 @@
+const WARRIOR_MAX_HEALTH = 20;
+
 class Player {
+
   playTurn(warrior) {
+
   	if( warrior.feel().isEmpty() ) {
-  		warrior.walk();
+
+  		if( warrior.health() < WARRIOR_MAX_HEALTH ) {
+  			warrior.rest()
+  		} else {
+  			warrior.walk();
+  		}
+
   	} else {
   		warrior.attack();
   	}
+
   }
+
 }
 
 global.Player = Player;
